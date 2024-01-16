c7bbe985b576fd7b041a5040c83c44de2722b4ca
blue32a <blue32a.jp@gmail.com>
2024-01-16 10:59:30 +0900

    スタック生成の重複を排除する ー スタック

--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StackTest {
+    private Stack stack = new Stack();
+
     @Test
     public void canCreateStack() throws Exception {
-        Stack stack = new Stack();
         assertTrue(stack.isEmpty());
     }
     @Test
     public void canPush() throws Exception {
-        Stack stack = new Stack();
         stack.push(0);
         assertFalse(stack.isEmpty());
     }
