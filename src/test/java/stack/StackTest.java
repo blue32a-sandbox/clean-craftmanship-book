789c5b225cf1e405f38622c79e7f02dfcfd48926
blue32a <blue32a.jp@gmail.com>
2024-01-16 10:42:52 +0900

    作成したスタックをアサートする ー スタック

--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -2,9 +2,12 @@ package stack;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StackTest {
     @Test
     public void canCreateStack() throws Exception {
         Stack stack = new Stack();
+        assertTrue(stack.isEmpty());
     }
 }
