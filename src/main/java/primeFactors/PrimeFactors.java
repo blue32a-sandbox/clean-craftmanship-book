944e1503ee748b4c4906c807f4a5f915204d0c88
blue32a <blue32a.jp@gmail.com>
2024-01-17 11:38:49 +0900

    テストを通すために一般化 ー 素因数分解

--- src/main/java/primeFactors/PrimeFactors.java
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -7,7 +7,7 @@ public class PrimeFactors {
     public List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
         if (n > 1) {
-            if (n % 2 == 0) {
+            while (n % 2 == 0) {
                 factors.add(2);
                 n /= 2;
             }
