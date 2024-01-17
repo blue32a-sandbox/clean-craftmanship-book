53be142d052474f347ec8ab645b1abdb6d509b67
blue32a <blue32a.jp@gmail.com>
2024-01-17 11:29:53 +0900

    nが2で割り切れるかどうかをテストしてみる ー 素因数分解

--- src/main/java/primeFactors/PrimeFactors.java
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -6,8 +6,13 @@ import java.util.List;
 public class PrimeFactors {
     public List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        if (n > 1)
+        if (n > 1) {
+            if (n % 2 == 0) {
+                factors.add(2);
+                n /= 2;
+            }
             factors.add(n);
+        }
         return factors;
     }
 }
