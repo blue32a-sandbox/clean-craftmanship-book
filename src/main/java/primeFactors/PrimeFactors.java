5eb84a752efae4b4aeba3de5d56521c40e8534cb
blue32a <blue32a.jp@gmail.com>
2024-01-17 11:18:57 +0900

    テストを通すためにif文を追加 ー 素因数分解

--- src/main/java/primeFactors/PrimeFactors.java
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class PrimeFactors {
     public List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
+        if (n > 1)
+            factors.add(2);
         return factors;
     }
 }
