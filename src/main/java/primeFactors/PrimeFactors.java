f0411f5130fe28134de5c8c1f9aed5d9e852e8f8
blue32a <blue32a.jp@gmail.com>
2024-01-17 11:22:07 +0900

    テストを通すために固定値を変数に変換 ー 素因数分解

--- src/main/java/primeFactors/PrimeFactors.java
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -7,7 +7,7 @@ public class PrimeFactors {
     public List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
         if (n > 1)
-            factors.add(2);
+            factors.add(n);
         return factors;
     }
 }
