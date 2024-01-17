4e5c91652a150b9b37e7943e0654ec64192f329c
blue32a <blue32a.jp@gmail.com>
2024-01-17 11:54:17 +0900

    リファクタリング ー 素因数分解

--- src/main/java/primeFactors/PrimeFactors.java
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -6,14 +6,11 @@ import java.util.List;
 public class PrimeFactors {
     public List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        int divisor = 2;
-        while (n > 1) {
-            while (n % divisor == 0) {
+
+        for (int divisor = 2; n > 1; divisor++)
+            for (; n % divisor == 0; n /= divisor)
                 factors.add(divisor);
-                n /= divisor;
-            }
-            divisor++;
-        }
+
         return factors;
     }
 }
