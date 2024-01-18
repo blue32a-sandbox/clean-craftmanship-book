fa7e200a53d4a1e1a61ffa9850a4728aa7139593
blue32a <blue32a.jp@gmail.com>
2024-01-18 10:03:15 +0900

    次に考えられる退化したテストを追加 ー ボウリングゲーム

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -19,4 +19,11 @@ public class BowlingTest {
             g.roll(0);
         assertEquals(0, g.score());
     }
+
+    @Test
+    public void allOnes() throws Exception {
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
 }
