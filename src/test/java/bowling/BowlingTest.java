ae20e5cff14b5e465d6d337918d0b67211c57a45
blue32a <blue32a.jp@gmail.com>
2024-01-18 09:58:29 +0900

    スコア計算のアサートをするため、最も単純で退化したゲームを投球する ー ボウリングゲーム

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -3,11 +3,20 @@ package bowling;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingTest {
     private Game g;
 
     @BeforeEach
     public void setUp() throws Exception {
         g = new Game();
+    }
+
+    @Test
+    public void gutterGame() throws Exception {
+        for (int i = 0; i < 20; i++)
+            g.roll(0);
+        assertEquals(0, g.score());
     }
 }
