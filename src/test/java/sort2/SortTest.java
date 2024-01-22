51f33d847ffeea86b49ebc441fb5b7634d5c9ad8
blue32a <blue32a.jp@gmail.com>
2024-01-22 15:31:32 +0900

    ３つの要素の残りの２ケースをテストする ー ソート２

--- src/test/java/sort2/SortTest.java
+++ src/test/java/sort2/SortTest.java
@@ -17,6 +17,8 @@ public class SortTest {
         assertEquals(asList(1, 2, 3), sorter.sort(asList(2, 1, 3)));
         assertEquals(asList(1, 2, 3), sorter.sort(asList(1, 3, 2)));
         assertEquals(asList(1, 2, 3), sorter.sort(asList(3, 2, 1)));
+        assertEquals(asList(1, 2, 3), sorter.sort(asList(2, 3, 1)));
+        assertEquals(asList(1, 2, 3), sorter.sort(asList(3, 1, 2)));
 //        assertEquals(asList(1, 2, 3, 4), sorter.sort(asList(1, 2, 3, 4)));
     }
 }
