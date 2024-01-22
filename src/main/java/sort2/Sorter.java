04c5f65ba000e261687a2939426986310f12efeb
blue32a <blue32a.jp@gmail.com>
2024-01-22 14:26:22 +0900

    テストを通すため正しい順番の要素を持つリストを作成 ー ソート２

--- src/main/java/sort2/Sorter.java
+++ src/main/java/sort2/Sorter.java
@@ -2,8 +2,19 @@ package sort2;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class Sorter {
     public List<Integer> sort(List<Integer> list) {
-        return list;
+        if (list.size() <= 1)
+            return list;
+        else {
+            int first = list.get(0);
+            int second = list.get(1);
+            if (first > second)
+                return asList(second, first);
+            else
+                return asList(first, second);
+        }
     }
 }
