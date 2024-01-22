88999e06b4ef102d835cbe221c8f89f62a5cc030
blue32a <blue32a.jp@gmail.com>
2024-01-22 12:30:15 +0900

    テストを通すためリストの長さを移動するループを追加 ー ソート１

--- src/main/java/sort1/Sorter.java
+++ src/main/java/sort1/Sorter.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class Sorter {
     public List<Integer> sort(List<Integer> list) {
         if (list.size() > 1) {
-            int firstIndex = 0;
-            int secondIndex = 1;
-            if (list.get(firstIndex) > list.get(secondIndex)) {
-                int first = list.get(firstIndex);
-                int second = list.get(secondIndex);
-                list.set(firstIndex, second);
-                list.set(secondIndex, first);
+            for (int firstIndex = 0; firstIndex < list.size() - 1; firstIndex++) {
+                int secondIndex = firstIndex + 1;
+                if (list.get(firstIndex) > list.get(secondIndex)) {
+                    int first = list.get(firstIndex);
+                    int second = list.get(secondIndex);
+                    list.set(firstIndex, second);
+                    list.set(secondIndex, first);
+                }
             }
         }
         return list;
