568409a5d0f2e56bafed094b28cba5d78dc424c7
blue32a <blue32a.jp@gmail.com>
2024-01-22 15:50:15 +0900

    最初のifを改良する ー ソート２

--- src/main/java/sort2/Sorter.java
+++ src/main/java/sort2/Sorter.java
@@ -8,15 +8,16 @@ import static java.util.stream.Collectors.*;
 
 public class Sorter {
     public List<Integer> sort(List<Integer> list) {
-        if (list.size() <= 1)
-            return list;
+        List<Integer> result = new ArrayList<>();
+
+        if (list.size() == 0)
+            return result;
         else {
             int middle = list.get(0);
             List<Integer> middles = list.stream().filter(x -> x == middle).collect(toList());
             List<Integer> lessers = list.stream().filter(x -> x < middle).collect(toList());
             List<Integer> greaters = list.stream().filter(x -> x > middle).collect(toList());
 
-            List<Integer> result = new ArrayList<>();
             result.addAll(sort(lessers));
             result.addAll(middles);
             result.addAll(sort(greaters));
