343ba1df7cf06b87585213aab11d6c1f654e11ac
blue32a <blue32a.jp@gmail.com>
2024-01-22 15:38:03 +0900

    ４つの要素のテストを追加 ー ソート２

--- src/test/java/sort2/SortTest.java
+++ src/test/java/sort2/SortTest.java
@@ -20,5 +20,7 @@ public class SortTest {
         assertEquals(asList(1, 2, 3), sorter.sort(asList(2, 3, 1)));
         assertEquals(asList(1, 2, 3), sorter.sort(asList(3, 1, 2)));
         assertEquals(asList(1, 2, 3, 4), sorter.sort(asList(1, 2, 3, 4)));
+        assertEquals(asList(1, 2, 3, 4), sorter.sort(asList(2, 1, 3, 4)));
+        assertEquals(asList(1, 2, 3, 4), sorter.sort(asList(4, 3, 2, 1)));
     }
 }
