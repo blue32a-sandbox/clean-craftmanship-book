b985459c8e56ce19fa0e8353dc8dc8e4b1a759f5
blue32a <blue32a.jp@gmail.com>
2024-01-22 12:34:14 +0900

    テストを通すため更に別のループを追加 ー ソート１

--- src/main/java/sort1/Sorter.java
+++ src/main/java/sort1/Sorter.java
@@ -6,13 +6,15 @@ import java.util.List;
 public class Sorter {
     public List<Integer> sort(List<Integer> list) {
         if (list.size() > 1) {
-            for (int firstIndex = 0; firstIndex < list.size() - 1; firstIndex++) {
-                int secondIndex = firstIndex + 1;
-                if (list.get(firstIndex) > list.get(secondIndex)) {
-                    int first = list.get(firstIndex);
-                    int second = list.get(secondIndex);
-                    list.set(firstIndex, second);
-                    list.set(secondIndex, first);
+            for (int limit = list.size() - 1; limit > 0; limit--) {
+                for (int firstIndex = 0; firstIndex < limit; firstIndex++) {
+                    int secondIndex = firstIndex + 1;
+                    if (list.get(firstIndex) > list.get(secondIndex)) {
+                        int first = list.get(firstIndex);
+                        int second = list.get(secondIndex);
+                        list.set(firstIndex, second);
+                        list.set(secondIndex, first);
+                    }
                 }
             }
         }
