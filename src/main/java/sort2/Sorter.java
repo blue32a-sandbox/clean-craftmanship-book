9b813bf7e7ac99f0e3989877603ccf8b8ec8c651
blue32a <blue32a.jp@gmail.com>
2024-01-22 15:37:15 +0900

    lessersとgreatersのリストを作成するためにフィルターを適用 ー ソート２

--- src/main/java/sort2/Sorter.java
+++ src/main/java/sort2/Sorter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
 
 public class Sorter {
     public List<Integer> sort(List<Integer> list) {
@@ -18,20 +19,9 @@ public class Sorter {
                 return asList(first, second);
         }
         else {
-            int first = list.get(1);
             int middle = list.get(0);
-            int last = list.get(2);
-            List<Integer> lessers = new ArrayList<>();
-            List<Integer> greaters = new ArrayList<>();
-
-            if (first < middle)
-                lessers.add(first);
-            if (last < middle)
-                lessers.add(last);
-            if (first > middle)
-                greaters.add(first);
-            if (last > middle)
-                greaters.add(last);
+            List<Integer> lessers = list.stream().filter(x -> x < middle).collect(toList());
+            List<Integer> greaters = list.stream().filter(x -> x > middle).collect(toList());
 
             List<Integer> result = new ArrayList<>();
             result.addAll(sort(lessers));
