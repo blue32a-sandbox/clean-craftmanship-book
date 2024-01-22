d8eb1f480420db65b2874f25e40de983bacad633
blue32a <blue32a.jp@gmail.com>
2024-01-22 15:46:22 +0900

    `list.size() == 2`のケースは不要なので削除 ー ソート２

--- src/main/java/sort2/Sorter.java
+++ src/main/java/sort2/Sorter.java
@@ -10,14 +10,6 @@ public class Sorter {
     public List<Integer> sort(List<Integer> list) {
         if (list.size() <= 1)
             return list;
-        else if (list.size() == 2) {
-            int first = list.get(0);
-            int second = list.get(1);
-            if (first > second)
-                return asList(second, first);
-            else
-                return asList(first, second);
-        }
         else {
             int middle = list.get(0);
             List<Integer> middles = list.stream().filter(x -> x == middle).collect(toList());
