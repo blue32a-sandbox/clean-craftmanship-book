14cc59f4fe175bbf3b84fed6f3523689cc90487c
blue32a <blue32a.jp@gmail.com>
2024-01-22 14:29:19 +0900

    正しい順番の３つの要素をテストする ー ソート２

--- src/main/java/sort2/Sorter.java
+++ src/main/java/sort2/Sorter.java
@@ -8,13 +8,16 @@ public class Sorter {
     public List<Integer> sort(List<Integer> list) {
         if (list.size() <= 1)
             return list;
-        else {
+        else if (list.size() == 2) {
             int first = list.get(0);
             int second = list.get(1);
             if (first > second)
                 return asList(second, first);
             else
                 return asList(first, second);
+        }
+        else {
+            return list;
         }
     }
 }
