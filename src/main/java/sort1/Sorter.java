75208956079b5ce76ba3db7db35aa15083c439f6
blue32a <blue32a.jp@gmail.com>
2024-01-22 12:10:41 +0900

    テストを通すために要素を交換する ー ソート１

--- src/main/java/sort1/Sorter.java
+++ src/main/java/sort1/Sorter.java
@@ -5,6 +5,14 @@ import java.util.List;
 
 public class Sorter {
     public List<Integer> sort(List<Integer> list) {
+        if (list.size() > 1) {
+            if (list.get(0) > list.get(1)) {
+                int first = list.get(0);
+                int second = list.get(1);
+                list.set(0, second);
+                list.set(1, first);
+            }
+        }
         return list;
     }
 }
