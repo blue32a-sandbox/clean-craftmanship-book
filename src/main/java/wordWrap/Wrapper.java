a4c6db14afe3be3145dfef8371fc6138cb4eb33f
blue32a <blue32a.jp@gmail.com>
2024-01-23 10:16:53 +0900

    テストを通すためにw番目から逆方向にスペースを検索 ー ワードラップ

--- src/main/java/wordWrap/Wrapper.java
+++ src/main/java/wordWrap/Wrapper.java
@@ -4,7 +4,12 @@ public class Wrapper {
     public String wrap(String s, int w) {
         if (w >= s.length())
             return s;
-        else
-            return s.substring(0, w).trim() + "\n" + wrap(s.substring(w).trim(), w);
+        else {
+            int br = s.lastIndexOf(" ", w);
+            if (br == -1)
+                br = w;
+            return s.substring(0, br).trim() + "\n" +
+                   wrap(s.substring(br).trim(), w);
+        }
     }
 }
