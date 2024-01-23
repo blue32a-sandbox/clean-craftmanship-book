e38093af8fc88653334609741074f01fca3a54a0
blue32a <blue32a.jp@gmail.com>
2024-01-23 10:10:59 +0900

    これらのテストは通る ー ワードラップ

--- src/test/java/wordWrap/WrapTest.java
+++ src/test/java/wordWrap/WrapTest.java
@@ -22,5 +22,14 @@ public class WrapTest {
         assertWrapped("xxx", 3, "xxx");
         assertWrapped("x x", 1, "x\nx");
         assertWrapped("x x", 2, "x\nx");
+        assertWrapped("x x", 3, "x x");
+        assertWrapped("x x x", 1, "x\nx\nx");
+        assertWrapped("x x x", 2, "x\nx\nx");
+        assertWrapped("x x x", 3, "x x\nx");
+        assertWrapped("x x x", 4, "x x\nx");
+        assertWrapped("x x x", 5, "x x x");
+        assertWrapped("xx xx", 1, "x\nx\nx\nx");
+        assertWrapped("xx xx", 2, "xx\nxx");
+        assertWrapped("xx xx", 3, "xx\nxx");
     }
 }
