88ebc41814467d200d312f3ed4e823399f2da91f
blue32a <blue32a.jp@gmail.com>
2024-01-24 16:45:02 +0900

    テストを通すためリストとループを追加し、レジストリ関係をRentalクラスに移動 ー ビデオストア

--- src/main/java/videoStore/Customer.java
+++ src/main/java/videoStore/Customer.java
@@ -1,33 +1,55 @@
 package videoStore;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static videoStore.VideoRegistry.*;
 import static videoStore.VideoRegistry.VideoType.*;
 
 public class Customer {
-    private String title;
-    private int days;
+    private List<Rental> rentals = new ArrayList<>();
 
     public void addRental(String title , int days) {
-        this.title = title;
-        this.days = days;
+        rentals.add(new Rental(title, days));
     }
 
     public int getRentalFee() {
-        if (VideoRegistry.getType(title) == REGULAR)
-            return applyGracePeriod(150, 3);
-        else
-            return 100 * days;
+        int fee = 0;
+        for (Rental rental : rentals) {
+            if (getType(rental.title) == REGULAR)
+                fee += applyGracePeriod(150, rental.days, 3);
+            else
+                fee += rental.days * 100;
+        }
+        return fee;
     }
 
     public int getRenterPoints() {
-        if (VideoRegistry.getType(title) == REGULAR)
-            return applyGracePeriod(1, 3);
-        else
-            return 1;
+        int points = 0;
+        for (Rental rental : rentals) {
+            if (getType(rental.title) == REGULAR)
+                points += applyGracePeriod(1, rental.days, 3);
+            else
+                points++;
+        }
+        return points;
     }
 
-    private int applyGracePeriod(int amount, int grace) {
+    private int applyGracePeriod(int amount, int days, int grace) {
         if (days > grace)
             return amount + amount * (days - grace);
         return amount;
+    }
+
+    public class Rental {
+        public String title;
+        public int days;
+        public VideoType type;
+
+        public Rental(String title, int days) {
+            this.title = title;
+            this.days = days;
+            type = VideoRegistry.getType(title);
+        }
     }
 }
