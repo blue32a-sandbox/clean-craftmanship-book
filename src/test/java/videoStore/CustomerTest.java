465f280d3b299293a314bb738a4547e4416fd58c
blue32a <blue32a.jp@gmail.com>
2024-01-24 11:35:10 +0900

    顧客のテストクラスを作り、最初のテストメソッドを追加 ー ビデオストア

---
+++ src/test/java/videoStore/CustomerTest.java
@@ -0,0 +1,15 @@
+package videoStore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerTest {
+    @Test
+    public void RegularMovie_OneDay() throws Exception {
+        Customer c = new Customer();
+        c.addRental("RegularMovie", 1);
+        assertEquals(1.5, c.getRentalFee(), 0.001);
+        assertEquals(1, c.getRenterPoints());
+    }
+}
