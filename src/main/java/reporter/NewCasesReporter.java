060474136719357d7c5564ad185bd5e7ba7921fb
blue32a <blue32a.jp@gmail.com>
2024-01-25 11:35:41 +0900

    抽出と名前の変更 ー レポート

--- src/main/java/reporter/NewCasesReporter.java
+++ src/main/java/reporter/NewCasesReporter.java
@@ -3,61 +3,94 @@ package reporter;
 import java.util.*;
 
 public class NewCasesReporter {
-    int totalCases;
-    Map<String, Integer> stateCounts = new HashMap<>();
-    List<County> counties = new ArrayList<>();
+    private int totalCases;
+    private final Map<String, Integer> stateCounts = new HashMap<>();
+    private final List<County> counties = new ArrayList<>();
 
     public String makeReport(String countyCsv) {
         String[] countyLines = countyCsv.split("\n");
         calculateCounties(countyLines);
 
-        StringBuilder report = new StringBuilder("" +
-                "County State Avg New Cases\n" +
-                "====== ===== =============\n");
-        for (County county : counties) {
-            report.append(String.format("%-11s%-10s%.2f\n",
-                    county.county,
-                    county.state,
-                    county.rollingAverage));
-        }
+        StringBuilder report = makeHeader();
+        report.append(makeCountyDetails());
         report.append("\n");
-        TreeSet<String> states = new TreeSet<>(stateCounts.keySet());
-        for (String state : states)
-            report.append(String.format("%s cases: %d\n",
-                    state, stateCounts.get(state)));
+        report.append(makeStateTotals());
         report.append(String.format("Total Cases: %d\n", totalCases));
         return report.toString();
     }
 
-    private void calculateCounties(String[] lines) {
+    private void calculateCounties(String[] countyLines) {
         totalCases = 0;
         stateCounts.clear();
         counties.clear();
-        for (String line : lines) {
-            String[] tokens = line.split(",");
-            County county = new County();
-            county.county = tokens[0].trim();
-            county.state = tokens[1].trim();
-            // compute rolling average
-            int lastDay = tokens.length - 1;
-            int firstDay = lastDay - 7 + 1;
-            if (firstDay < 2)
-                firstDay = 2;
-            double n = lastDay - firstDay + 1;
-            int sum = 0;
-            for (int day = firstDay; day <= lastDay; day++)
-                sum += Integer.parseInt(tokens[day].trim());
-            county.rollingAverage = (sum / n);
-
-            // compute sum of cases.
-            int cases = 0;
-            for (int i = 2; i < tokens.length; i++)
-                cases += (Integer.parseInt(tokens[i].trim()));
-            totalCases += cases;
-            int stateCount = stateCounts.getOrDefault(county.state, 0);
-            stateCounts.put(county.state, stateCount + cases);
-            counties.add(county);
+        for (String countyLine : countyLines) {
+            counties.add(calculateCounty(countyLine));
         }
+    }
+
+    private County calculateCounty(String line) {
+        County county = new County();
+        String[] tokens = line.split(",");
+        county.county = tokens[0].trim();
+        county.state = tokens[1].trim();
+
+        county.rollingAverage = calculateRollingAverage(tokens);
+
+        int cases = calculateSumOfCases(tokens);
+        totalCases += cases;
+        incrementStateCounter(county.state, cases);
+
+        return county;
+    }
+
+    private double calculateRollingAverage(String[] tokens) {
+        int lastDay = tokens.length - 1;
+        int firstDay = lastDay - 7 + 1;
+        if (firstDay < 2)
+            firstDay = 2;
+        double n = lastDay - firstDay + 1;
+        int sum = 0;
+        for (int day = firstDay; day <= lastDay; day++)
+            sum += Integer.parseInt(tokens[day].trim());
+        return (sum / n);
+    }
+
+    private int calculateSumOfCases(String[] tokens) {
+        int cases = 0;
+        for (int i = 2; i < tokens.length; i++)
+            cases += (Integer.parseInt(tokens[i].trim()));
+        return cases;
+    }
+
+    private void incrementStateCounter(String state, int cases) {
+        int stateCount = stateCounts.getOrDefault(state, 0);
+        stateCounts.put(state, stateCount + cases);
+    }
+
+    private StringBuilder makeHeader() {
+        return new StringBuilder("" +
+                "County State Avg New Cases\n" +
+                "====== ===== =============\n");
+    }
+
+    private StringBuilder makeCountyDetails() {
+        StringBuilder countyDetails = new StringBuilder();
+        for (County county : counties) {
+            countyDetails.append(String.format("%-11s%-10s%.2f\n",
+                    county.county,
+                    county.state,
+                    county.rollingAverage));
+        }
+        return countyDetails;
+    }
+
+    private StringBuilder makeStateTotals() {
+        StringBuilder stateTotals = new StringBuilder();
+        TreeSet<String> states = new TreeSet<>(stateCounts.keySet());
+        for (String state : states)
+            stateTotals.append(String.format("%s cases: %d\n",
+                    state, stateCounts.get(state)));
+        return stateTotals;
     }
 
     public static class County {
